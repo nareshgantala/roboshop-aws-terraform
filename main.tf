e3d4e965116913a81937d0e1273d27babf5d4f1e
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 12:22:43 +0530

    copy private key file commented

--- main.tf
+++ main.tf
@@ -20,17 +20,17 @@ module "dns"{
 }
 
 
-resource "null_resource" "name" {
-  depends_on = [ module.ec2, module.dns ]
-  for_each = var.component
-  connection {
-    type = "ssh"
-    user = "ec2-user"
-    private_key = "C:\\Users\\DELL\\Downloads\\roboshop_pem.pem"
-    host = module.ec2[each.key].private_ip
-  }
-  provisioner "file" {
-    source = "C:\\Users\\DELL\\Downloads\\roboshop_pem.pem"
-    destination = "/home/ec2-user/roboshop_pem.pem"
-  }
-}
+# resource "null_resource" "name" {
+#   depends_on = [ module.ec2, module.dns ]
+#   for_each = var.component
+#   connection {
+#     type = "ssh"
+#     user = "ec2-user"
+#     private_key = "/home/ec2-user/roboshop_pem.pem"
+#     host = module.ec2[each.key].private_ip
+#   }
+#   provisioner "file" {
+#     source = "/home/ec2-user/roboshop_pem.pem"
+#     destination = "/home/ec2-user/roboshop_pem.pem"
+#   }
+# }
