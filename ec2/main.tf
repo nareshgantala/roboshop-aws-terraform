47605accfb7737cabec03addf0aa2cc664b60953
Naresh Gantala <nareshgantala@gmail.com>
2026-05-20 13:26:58 +0530

    up

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "main" {
   ami           = var.ami_id
   instance_type = var.size
+  key_name = "roboshop_pem"
   subnet_id     = var.instance_subnet_id
   associate_public_ip_address = true
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
