cbb96e3e0b13296cd43ee18974d0efd83194ce3b
Naresh Gantala <nareshgantala@gmail.com>
2026-05-21 19:24:46 +0530

    provider added

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    backend "s3" {
+    bucket = "roboshop-aws-terraform"
+    key    = "infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
