5f88e3ebab3b7d2942e0d6be447592a6a66013b1
Naresh Gantala <nareshgantala@gmail.com>
2026-05-22 17:00:32 +0530

    null resource update

--- main.tf
+++ main.tf
@@ -23,16 +23,19 @@ module "dns"{
 resource "null_resource" "name" {
   depends_on = [ module.ec2, module.dns ]
   for_each = var.component
+  triggers = {
+    always_run = timestamp()
+  }
   connection {
     type = "ssh"
     user = "ec2-user"
-    private_key = "/home/ec2-user/roboshop_pem.pem"
+    private_key = file("/home/ec2-user/roboshop_pem.pem")
     host = module.ec2[each.key].private_ip
   }
   provisioner "remote-exec" {
     inline = [ 
       "sudo dnf install ansible-core",
-      "ansible pull -i localhost, -U https://github.com/nareshgantala/roboshop-aws-ansible.git ${each.key}.yml "
+      "ansible-pull -i localhost, -U https://github.com/nareshgantala/roboshop-aws-ansible.git ${each.key}.yml "
      ]
 
 
